67091f4c10e4f8055e657a92e6079534edabae72
zjiawei <weijian45590@bill-jc.com>
2021-01-26 10:57:49 +0800

    update RstFinTool

--- tools/RstFinTool/RstFinTool.cpp
+++ tools/RstFinTool/RstFinTool.cpp
@@ -27,27 +27,27 @@ bool IS_HELP=false;
 bool IS_SERVER=false;
 bool IS_RST=true;
 bool IS_FIN=false;
-std::string SERV_IP=("0.0.0.0");
-uint32_t SERV_PORT=8001;
-std::string CLT_IP=("0.0.0.0");
-uint32_t CLT_PORT=8989;
+std::string SRC_IP=("0.0.0.0");
+uint32_t SRC_PORT=8888;
+std::string DST_IP=("0.0.0.0");
+uint32_t DST_PORT=6666;
 
 void showUsage() {
-    cout << "RstFinTool Usage:" << endl;
+    cout << "RstFinTool: simulate the TCP server send a RST/FIN packet to client after 3-handshakes complete." << endl;
+    cout << "1) Usage:" << endl;
     cout << " -h, --help                Print this message and exit." << endl;
     cout << " -s, --server              Run the RstFinTool as tcp server. default run as client." << endl;
     cout << " -r, --rst                 The RstFinTool send a RST packes to the client after 3-handshakes complete, that is select when unspecified." << endl;
     cout << " -f, --fin                 The RstFinTool send a FIN packes to the client after 3-handshakes complete." << endl;
-    cout << " -i, --ip                  String, the server  IP addrss, must be specified. default as 0.0.0.0." << endl;
-    cout << " -p, --port                Int, the server port, must be specified. default as 8001." << endl;
-    cout << " -c, --sip                 String, the client IP addrss, must be specified. default as 0.0.0.0." << endl;
-    cout << " -d, --sport               Int, the client port, must be specified. default as 8989." << endl;
-    cout << "RstFinTool Examples:" << endl;
-    cout << " ./RstFinTool -s -r -i 127.0.0.1 -p 6666  # Run RstFinTool as RST server listen on 127.0.0.1:6666" << endl;
-    cout << " ./RstFinTool -s -f -i 127.0.0.1 -p 6666  # Run RstFinTool as FIN server listen on 127.0.0.1:6666" << endl;
-    cout << " ./RstFinTool -r -i 127.0.0.1 -p 6666     # Send data to RST server, that listen on 127.0.0.1:6666" << endl;
-    cout << " ./RstFinTool -f -i 127.0.0.1 -p 6666     # Send data to FIN server, that listen on 127.0.0.1:6666" << endl;
-    cout << " ./RstFinTool -f -c 127.0.0.1 -d 8888 -i 127.0.0.1 -p 6666     # the Client bind on 127.0.0.1:8888 and send data to FIN server, that listen on 127.0.0.1:6666" << endl;
+    cout << " -a, --sip                 String, the source IP addrss, must be specified. default as 0.0.0.0." << endl;
+    cout << " -b, --sport               Int, the source port, must be specified. default as 6666." << endl;
+    cout << " -c, --dip                 String, the destination IP addrss, must be specified. default as 0.0.0.0." << endl;
+    cout << " -d, --dport               Int, the destination port, must be specified. default as 8888." << endl;
+    cout << "2) Examples:" << endl;
+    cout << " ./RstFinTool --server --rst --sip 127.0.0.1 --sport 8888  # Run RstFinTool as RST server listen on 127.0.0.1:8888" << endl;
+    cout << " ./RstFinTool --server --fin --sip 127.0.0.1 --sport 8888  # Run RstFinTool as FIN server listen on 127.0.0.1:8888" << endl;
+    cout << " ./RstFinTool --fin --sip 127.0.0.1 --sport 6666 --dip 127.0.0.1 --dport 8888" << endl;
+    cout << "\t\t\t\t# The client bind on 127.0.0.1:6666 and send data to FIN server, that listen on 127.0.0.1:8888" << endl;
 }
 
 int parseOpt(int argc, char *argv[]) {
@@ -56,16 +56,16 @@ int parseOpt(int argc, char *argv[]) {
       {"server", no_argument, NULL, 's'},
       {"rst", no_argument, NULL, 'r'},
       {"fin", no_argument, NULL, 'f'},
-      {"ip", required_argument, NULL, 'i'},
-      {"port", required_argument, NULL, 'p'},
-      {"sip", optional_argument, NULL, 'c'},
-      {"sport", optional_argument, NULL, 'd'}
+      {"sip", required_argument, NULL, 'a'},
+      {"sport", required_argument, NULL, 'b'},
+      {"dip", required_argument, NULL, 'c'},
+      {"dport", required_argument, NULL, 'd'}
    };
 
    int optIndex = 0;
    for (;;) 
    {
-      optIndex = getopt_long(argc, argv, "hsrf:i:p:c:d:", longOpts, NULL);
+      optIndex = getopt_long(argc, argv, "hsrf:a:b:c:d:", longOpts, NULL);
       if (-1 == optIndex)
       {
          break;
@@ -83,17 +83,17 @@ int parseOpt(int argc, char *argv[]) {
                IS_RST = false;
                IS_FIN = true;
                break;
-         case 'i':
-               SERV_IP = std::string(optarg);
+         case 'a':
+               SRC_IP = std::string(optarg);
                break;
-         case 'p':
-               SERV_PORT = atoi(optarg);
+         case 'b':
+               SRC_PORT = atoi(optarg);
                break;
          case 'c':
-               CLT_IP = std::string(optarg);
+               DST_IP = std::string(optarg);
                break;
          case 'd':
-               CLT_PORT = atoi(optarg);
+               DST_PORT = atoi(optarg);
                break;
          case 'h':
          default:
@@ -112,7 +112,7 @@ int handleAccept(int fd)
       char pcContent[4096];
       ::bzero(pcContent, sizeof(pcContent));
       ::read(fd,pcContent, 4096);
-      printf("close the fd=%d for RST\n", fd);
+      printf("send RST packet to client ok\n\n", fd);
    }
 
    if (IS_FIN)
@@ -120,7 +120,7 @@ int handleAccept(int fd)
       char pcContent[128];
       ::bzero(pcContent, sizeof(pcContent));
       ::read(fd,pcContent, 128);
-      printf("close the fd=%d for FIN\n", fd);
+      printf("send FIN packet to client ok\n\n", fd);
    }
   
    ::close(fd);
@@ -141,11 +141,11 @@ int startServer()
 
    ::bzero(&listen_addr,sizeof(listen_addr));
    listen_addr.sin_family = AF_INET;
-   listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-   listen_addr.sin_port = htons(SERV_PORT);
+   listen_addr.sin_addr.s_addr = inet_addr(SRC_IP.c_str());    // INADDR_ANY
+   listen_addr.sin_port = htons(SRC_PORT);
    ::bind(listen_fd,(struct sockaddr *)&listen_addr, len);
    ::listen(listen_fd, 9999);
-   printf("tcp server listen on 0.0.0.0:%d, type=%s, fd=%d\n", SERV_PORT, IS_RST ? "RST" : "FIN", listen_fd);
+   printf("tcp server listen on %s:%d, type=%s, fd=%d\n", SRC_IP.c_str(), SRC_PORT, IS_RST ? "RST" : "FIN", listen_fd);
 
    while(1)
    {
@@ -161,12 +161,11 @@ int startServer()
          char ipDotDec[INET_ADDRSTRLEN];
          ::bzero(ipDotDec, sizeof(ipDotDec));
          ::inet_ntop(AF_INET, &(client_addr.sin_addr), ipDotDec, sizeof(ipDotDec));
-         printf("**************************************************\n");
-         printf("connection fd=%d, client address=%s:%d\n", client_fd, ipDotDec, port);
+         printf("*********client[%s:%d] connected, fd=%d*********\n", ipDotDec, port, client_fd);
       }
 
-      std::thread c(handleAccept, client_fd);
-      c.detach();
+      std::thread t(handleAccept, client_fd);
+      t.detach();
    }
 
    ::close(listen_fd);
@@ -184,9 +183,10 @@ int startClient()
 
    struct sockaddr_in s_addr, d_addr;
    socklen_t len = sizeof(s_addr);
-   s_addr.sin_addr.s_addr = inet_addr(CLT_IP.c_str());
-   s_addr.sin_port = htons(CLT_PORT);
-   if (bind(send_fd, (struct sockaddr *) &s_addr, sizeof(s_addr)) < 0) 
+   s_addr.sin_family = AF_INET;
+   s_addr.sin_addr.s_addr = inet_addr(SRC_IP.c_str());
+   s_addr.sin_port = htons(SRC_PORT);
+   if (bind(send_fd, (struct sockaddr *) &s_addr, sizeof(s_addr)) < 0)
    {
       perror("bind failed");
       return -1;
@@ -194,10 +194,10 @@ int startClient()
 
    bzero(&d_addr, sizeof(d_addr));
    d_addr.sin_family = AF_INET;
-   inet_pton(AF_INET, SERV_IP.c_str(), &d_addr.sin_addr);
-   d_addr.sin_port = htons(SERV_PORT);
+   inet_pton(AF_INET, DST_IP.c_str(), &d_addr.sin_addr);
+   d_addr.sin_port = htons(DST_PORT);
    if(connect(send_fd, (struct sockaddr*)&d_addr, len) == -1) 
-   { 
+   {
       perror("connect fail");
       return -1;
    }
@@ -206,15 +206,15 @@ int startClient()
    {
       // IP fragmentation
       char pcContent[5000]={0};
-      ::write(send_fd,pcContent,5000);
-      printf("send data for RST\n");
+      ::write(send_fd, pcContent,5000);
+      printf("client[%s:%d] send data to server[%s:%d] for RST\n", SRC_IP.c_str(), SRC_PORT, DST_IP.c_str(), DST_PORT);
    }
 
    if (IS_FIN)
    {
       char pcContent[16]={0};
-      ::write(send_fd,pcContent,16);
-      printf("send data for FIN\n");
+      ::write(send_fd, pcContent,16);
+      printf("client[%s:%d] send data to server[%s:%d] for FIN\n", SRC_IP.c_str(), SRC_PORT, DST_IP.c_str(), DST_PORT);
    }
 
    ::sleep(1);
